3daa4b3ed54c010e70af81c186351b038f086765
zakirashirli <117630695+zakirashirli@users.noreply.github.com>
2025-11-21 21:23:19 +0400

    movie project created

---
+++ My-Movie/src/test/java/com/movie/dea/MyMovieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.movie.dea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyMovieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
